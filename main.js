6f705808b52e93c512b2e40c895dec1e0fd41abd
Orazio <orazioventrella@hotmail.it>
2021-11-05 15:35:23 +0100

    analisi

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro javascript in cui:
+- Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+nome autore,
+foto profilo,
+data,
+testo del post,
+immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
+numero di likes.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+- Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
+*/
+
+// 1. creare un elenco di post tramite un'arrey con al suo interno degli oggetti => arrey di oggetti
+// 2. ogni oggetto dovrà contenere diverse informazioni/proprietà
+//   2.1 nome autore, foto profilo, data, testo del post, immagine, numero di likes
+//   2.2 non tutti i post devono avere una immagine
+// 3. stampare i post creati nel dom tramite js => ciclo for
+// 4. ad ogni click del tanto “Mi Piace” incrementare il numero di likes => addEventListener("click",
+
